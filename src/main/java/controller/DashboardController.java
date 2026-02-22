496bae445c235556a75d87946f8363e97c8c7ab1
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-02-22 13:31:15 +0530

    Enabled navigation to Manage Books screen

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,26 +2,38 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class DashboardController {
 
     @FXML
-    void btnManageBooksOnAction(ActionEvent event) {
-        System.out.println("Clicked Manage Books!");
+    void btnManageBooksOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/manage_books_form.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     @FXML
     void btnCustomersOnAction(ActionEvent event) {
-        System.out.println("Clicked Customers!");
+        System.out.println("Customers");
     }
 
     @FXML
     void btnRentalsOnAction(ActionEvent event) {
-        System.out.println("Clicked Rentals!");
+        System.out.println("Rentals");
     }
 
     @FXML
-    void btnLogoutOnAction(ActionEvent event) {
-        System.out.println("Clicked Logout");
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/login_form.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
